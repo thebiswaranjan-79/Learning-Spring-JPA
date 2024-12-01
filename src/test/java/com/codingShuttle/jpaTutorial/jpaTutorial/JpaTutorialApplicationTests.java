083f08014f3b15f7927cb9f85b9f6a16490a25d6
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-12-02 00:21:31 +0530

    first commit

---
+++ src/test/java/com/codingShuttle/jpaTutorial/jpaTutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.jpaTutorial.jpaTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
