0f01174725e86ad7b37da3a2fe548568efbf89df
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-12-02 01:05:37 +0530

    Learn about Key Features of JPA

---
+++ src/main/java/com/codingShuttle/jpaTutorial/jpaTutorial/entities/Product.java
@@ -0,0 +1,47 @@
+package com.codingShuttle.jpaTutorial.jpaTutorial.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product_table",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "sku_unique", columnNames = {"sku"}),
+        @UniqueConstraint(name = "title_price_unique", columnNames = {"title_x", "price"})
+        },
+        indexes = {
+            @Index(name = "sku_index", columnList = "sku")
+        }
+
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String sku;
+
+    @Column(name = "title_x")
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test.product_table (price, quantity, sku, title_x)
+VALUES
+(499.99, 75, 'LG-OLED55CX', 'LG OLED55CX Alexa Built-in 55-Inch 4K Smart TV'),
+(299.99, 120, 'SAMSUNG-UN55TU8000', 'SAMSUNG UN55TU8000 55-Inch 4K UHD Smart TV'),
+(699.99, 50, 'SONY-KD55X80J', 'SONY KD55X80J 55-Inch 4K Ultra HD LED Smart TV'),
+(149.99, 200, 'VIZIO-D24f-G1', 'VIZIO D24f-G1 24-Inch 1080p LED TV');
